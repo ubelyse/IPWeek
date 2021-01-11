1a8ff0f164dd3c55ccfaec87cb35cfd73f2ecbd2
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 15:12:33 +0200

    designing hotel activity

--- app/src/main/java/com/example/hotelreserve/HotelActivity.java
+++ app/src/main/java/com/example/hotelreserve/HotelActivity.java
@@ -1,13 +1,21 @@
 package com.example.hotelreserve;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HotelActivity extends AppCompatActivity {
     private static final String TAG = HotelActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel);
+        ButterKnife.bind(this);
+    }
 }
