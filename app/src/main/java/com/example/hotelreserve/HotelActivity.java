53c57d8307c4eb173dd537cb659855b45ad5fdf3
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 15:17:03 +0200

    added progress bar

--- app/src/main/java/com/example/hotelreserve/HotelActivity.java
+++ app/src/main/java/com/example/hotelreserve/HotelActivity.java
@@ -1,6 +1,8 @@
 package com.example.hotelreserve;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,11 +13,15 @@ import butterknife.ButterKnife;
 public class HotelActivity extends AppCompatActivity {
     private static final String TAG = HotelActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
         ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
     }
 }
