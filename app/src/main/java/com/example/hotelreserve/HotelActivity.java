11c5780b052ac725e50e9e6a5d39b4c2e759f87a
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 15:04:16 +0200

    designing hotel activity

---
+++ app/src/main/java/com/example/hotelreserve/HotelActivity.java
@@ -0,0 +1,13 @@
+package com.example.hotelreserve;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import butterknife.BindView;
+
+public class HotelActivity extends AppCompatActivity {
+    private static final String TAG = HotelActivity.class.getSimpleName();
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+
+}
