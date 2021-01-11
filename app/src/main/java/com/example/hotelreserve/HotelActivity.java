34a5858c49450ab7e02a647174785f9dfcc9ef2e
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 16:30:23 +0200

    edited hotel activity

--- app/src/main/java/com/example/hotelreserve/HotelActivity.java
+++ app/src/main/java/com/example/hotelreserve/HotelActivity.java
@@ -2,20 +2,30 @@ package com.example.hotelreserve;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HotelActivity extends AppCompatActivity {
     private static final String TAG = HotelActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
+
+    public List<Business> hotels;
+    private HotelsListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +37,49 @@ public class HotelActivity extends AppCompatActivity {
         String location = intent.getStringExtra("location");
 
         YelpApi client = YelpClient.getClient();
+
+        Call<YelpBusinessesSearchResponse> call=client.getHotels(location,"hotels");
+
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                hideProgressBar();
+
+                if (response.isSuccessful()){
+                    hotels= response.body().getBusinesses();
+                    mAdapter=new HotelsListAdapter(HotelActivity.this,hotels);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HotelActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showHotels();
+                }else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                hideProgressBar();
+                showFailureMessage();
+            }
+        });
+    }
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+    private void showHotels() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/com/example/hotelreserve/HotelsListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.hotelreserve;
+
+public class HotelsListAdapter {
+}
