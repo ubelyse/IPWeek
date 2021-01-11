318287e84fa6e53de64a9f7692ffcb25d24c5f39
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 20:00:03 +0200

    edited the api

--- app/src/main/java/com/example/hotelreserve/Constants.java
+++ app/src/main/java/com/example/hotelreserve/Constants.java
@@ -1,7 +1,7 @@
 package com.example.hotelreserve;
 
 public class Constants {
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_BASE_URL = "https://api.hoteling.org/3/";
     public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
 
 }
--- app/src/main/java/com/example/hotelreserve/network/YelpApi.java
+++ app/src/main/java/com/example/hotelreserve/network/YelpApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.Query;
 
 public interface YelpApi {
 
-    @GET("businesses/search")
+    @GET("businesses/popular?api_key=GfNSmB2VXmIYk9CEkgEdItPoBPgPB1FvK4KsHxV6aGgdZvfORtpFBhvu3SrkZmh1kFJmM0AO8mh6us9m4-j_0PbXqSvTrcmtorSNjWGfvzRzpr-5K88pCAnHYgX7X3Yx\"")
     Call<YelpBusinessesSearchResponse> getHotels(
             @Query("location") String location,
             @Query("term") String term
