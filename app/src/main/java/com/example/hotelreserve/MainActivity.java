b7314e30c6757e2dd4b27737c8a9187da73adf5b
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-11 14:55:08 +0200

    completed designing and function of main activity

--- app/src/main/java/com/example/hotelreserve/MainActivity.java
+++ app/src/main/java/com/example/hotelreserve/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.hotelreserve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.findHotel) Button mhotel;
+    @BindView(R.id.locationEditText)
+    EditText mlocation;
+    @BindView(R.id.appNameTextView)
+    TextView mappname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mhotel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mhotel) {
+            String location = mlocation.getText().toString();
+            Intent intent = new Intent(MainActivity.this, HotelActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
