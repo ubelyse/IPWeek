e75149558765d1c16e6b49b663da30bb022c4590
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-13 22:08:41 +0200

    used food api

--- app/src/main/java/com/example/hotelreserve/ui/HotelDetailFragment.java
+++ app/src/main/java/com/example/hotelreserve/ui/HotelDetailFragment.java
@@ -2,6 +2,8 @@ package com.example.hotelreserve.ui;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +20,7 @@ import org.parceler.Parcels;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HotelDetailFragment extends Fragment {
+public class HotelDetailFragment extends Fragment implements View.OnClickListener{
     @BindView(R.id.hotelimage) ImageView mImageLabel;
     @BindView(R.id.hotelnametxt) TextView mNameLabel;
     @BindView(R.id.hoteltype) TextView mCategoriesLabel;
@@ -61,6 +63,30 @@ public class HotelDetailFragment extends Fragment {
         mRatingLabel.setText(Double.toString(mhotels.getRating()) + "/5");
         mPhoneLabel.setText(mhotels.getPhone());
         mAddressLabel.setText(android.text.TextUtils.join(", ", mhotels.getAddress()));
+
+        mWebsiteLabel.setOnClickListener(this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v == mWebsiteLabel){
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(mhotels.getWebsite()));
+            startActivity(webIntent);
+        }
+        if (v == mPhoneLabel) {
+            Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
+                    Uri.parse("tel:" + mhotels.getPhone()));
+            startActivity(phoneIntent);
+        }if (v == mAddressLabel) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("geo:" + mhotels.getLatitude()
+                            + "," + mhotels.getLongitude()
+                            + "?q=(" + mhotels.getName() + ")"));
+            startActivity(mapIntent);
+        }
     }
 }
